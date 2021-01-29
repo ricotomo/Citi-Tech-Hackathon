c9148c06e7ff8fe50fdc404b30320efa591ed958
hayuh <hyuh@andrew.cmu.edu>
2021-01-29 18:11:15 -0500

    Started HTML of investor log in page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   print("Handling request to home page.")
-  return "Hello Hexuan, Halannah and Jonathan!"
+  return render_template('investor_logged.html')
 
 if __name__ == "__main__":
   app.run()
