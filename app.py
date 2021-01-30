dedb3612d58a295aa32e8cfa4d6eb794442d7dc1
hayuh <hyuh@andrew.cmu.edu>
2021-01-30 12:34:02 -0500

    student homepage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
   print("Handling request to home page.")
-  return render_template('investor_logged.html')
+  return render_template('student_homepage.html')
 
 if __name__ == "__main__":
   app.run()
