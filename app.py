746a0033a915039e19a963a52d775d19869b5554
Moritz Walther <friedicecream1@gmail.com>
2021-01-30 20:54:05 +0100

    Form handler
    Set up working form handler in Flask API awhich accepts the ISA signup form

--- app.py
+++ app.py
@@ -13,10 +13,17 @@ def investor_landing():
    else:
       return render_template('student_homepage.html')
 
-@app.route('/ISA_form',methods = ['GET'])
+@app.route('/ISA_form',methods = ['POST','GET'])
 def ISA_form():
    if request.method == 'GET':
       return render_template('ISA_form.html')
+   elif request.method=='POST':
+      tuition = request.form.get('tuition')
+      college = request.form.get('college')
+      major = request.form.get('major')
+      degree = request.form.get('degree')
+      print("testing accessing form elements " + tuition + " " + college)
+      return render_template('student_homepage.html')
    else:
       return render_template('student_homepage.html')
 
